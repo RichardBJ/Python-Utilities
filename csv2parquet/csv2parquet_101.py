7c9d01d3ade2d8cbe4ed90860bb08f9c2807594c
RichardBJ <rbj@liverpool.ac.uk>
2024-04-25 16:28:16 +0100

    AddColumns and csv2parquat OFFICE262 mods
    So added a DROP column to the add column subrepro
    and modifed cvs2parq main app to work better, Still concerned it is ADDING an index to the output files sometimes! (hence adding the drop column function).

--- csv2parquet/csv2parquet_101.py
+++ csv2parquet/csv2parquet_101.py
@@ -3,13 +3,15 @@
 Created on Wed Feb 28 17:56:28 2024
 SIMPLE CONVERSION BOTHWAYS between parquet and csv
 @author: rbj
-straight forward conversion to and from parquet and csv
+straight forward conversion to and from parquet and csv Should include txt with either tab or spaces. multiple spaces treated as one.
 Formatting all retained.
+
+SEEMS TO CREATE AN INDEX THIS NEEDS SORTING!!!!
 """
 
 import pandas as pd
 from tkinter import Tk
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilenames
 
 # Create the root Tk window
 root = Tk()
@@ -21,21 +23,35 @@ root.withdraw()
 root.lift()
 
 # Open a file dialog for CSV or Parquet files
-file_path = askopenfilename(filetypes=[("CSV files", "*.csv"), ("Parquet files", "*.parquet")])
+file_paths = askopenfilenames(
+    filetypes=[("CSV files", "*.csv"),
+               ("Parquet files", "*.parquet"),
+               ("txt files", "*.txt")])
 # Check if a file was selected
-if file_path:
-    # Determine the file type
-    if file_path.endswith('.csv'):
-        # Read the CSV file with pandas
-        df = pd.read_csv(file_path)
-        # Convert to Parquet and save
-        df.to_parquet(file_path.replace(".csv", ".parquet"))
-    elif file_path.endswith('.parquet'):
-        # Read the Parquet file with pandas
-        df = pd.read_parquet(file_path)
-        df.to_csv(file_path.replace(".parquet", ".csv"))
-    print(df.info())
-    print(df.head())
+
+for file_path in file_paths:
+    if file_path:
+        # Determine the file type
+        if file_path.endswith('.csv'):
+            # Read the CSV file with pandas
+            df = pd.read_csv(file_path)
+            # Convert to Parquet and save
+            df.to_parquet(file_path.replace(".csv", ".parquet"))
+        elif file_path.endswith('.txt'):
+            # Read the tx file with pandas
+            try:
+                df = pd.read_csv(file_path, sep='\t',
+                                 header=None)
+            except:
+                df = pd.read_csv(file_path, sep='\\s+',
+                                 header=None)
+            df.to_parquet(file_path.replace(".txt", ".parquet"))
+        elif file_path.endswith('.parquet'):
+            # Read the Parquet file with pandas
+            df = pd.read_parquet(file_path)
+            df.to_csv(file_path.replace(".parquet", ".csv"))
+        print(df.info())
+        print(df.head())
 
 
 
