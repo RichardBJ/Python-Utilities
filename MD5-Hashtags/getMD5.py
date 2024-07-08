decbf17deb7bc96690ec035d1a30066344fb06f8
RichardBJ <rbj@liverpool.ac.uk>
2024-07-08 12:40:32 +0100

    Making new MD5 hashtag scripts

---
+++ MD5-Hashtags/getMD5.py
@@ -0,0 +1,16 @@
+import hashlib, glob
+import pandas as pd
+
+files = glob.glob(r"data/*gz", recursive=True)
+
+table={}
+for row, file in enumerate(files):
+    print(file)
+    table[row]=[file,hashlib.md5(open(file,'rb').
+                            read()).hexdigest()]
+							
+tabledf=pd.DataFrame.from_dict(table, 
+        orient='index',columns=['File', 'MD5'])
+tabledf.head()
+
+tabledf.to_csv("hashtags.csv",mode="a")
