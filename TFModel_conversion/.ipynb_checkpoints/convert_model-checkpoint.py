ec8175feace35391f287e7956796adea6e92b2c1
RichardBJ <rbj@liverpool.ac.uk>
2024-10-20 13:48:58 +0100

    Json model comparison in tfConversions folder

---
+++ TFModel_conversion/.ipynb_checkpoints/convert_model-checkpoint.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 10 11:56:06 2024
+
+@author: rbj
+"""
+from tensorflow import keras
+import tensorflowjs as tfjs
+import h5py
+
+#Take a look at what we've got
+with h5py.File("model.h5", "r") as f:
+    print(list(f.keys()))
+
+model = keras.models.load_model("model.h5")
+
+# Convert the Keras model to TensorFlow.js format
+tfjs_target_dir = "json_model/tfjs_model"
+tfjs.converters.save_keras_model(model, tfjs_target_dir)
+
+print(f"Model converted and saved for TensorFlow.js at: {tfjs_target_dir}")
