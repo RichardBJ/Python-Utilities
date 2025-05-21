12791f6a216deb085ab785c8c761427368d6e531
Barrett-Jolley <rbj@liverpool.ac.uk>
2025-05-21 15:06:45 +0100

    changes to the converter and the concater; parquet...

---
+++ csv2parquet/convert2parquet.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import os
+from pathlib import Path
+
+si=0.01
+
+def convert_csvs():
+   for path in Path('.').rglob('*.csv'):
+       df = pd.read_csv(path)
+       df["Time"] = np.arange(0, len(df)) * si
+       parquet_path = path.with_suffix('.parquet')
+       df.to_parquet(parquet_path)
+       print(f'Converted {path} to {parquet_path}')
+
+convert_csvs()
